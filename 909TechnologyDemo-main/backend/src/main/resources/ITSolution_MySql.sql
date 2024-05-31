377e47422cf8d8d4bf4cded9bd5b473ba5d0e6a8
abhishek bhatt <abhi.bhatt1857@gmail.com>
2024-05-31 19:11:53 +0530

    updated

--- 909TechnologyDemo-main/backend/src/main/resources/ITSolution_MySql.sql
+++ 909TechnologyDemo-main/backend/src/main/resources/ITSolution_MySql.sql
@@ -6,11 +6,12 @@ id int auto_increment not null,
 name varchar(30),
 age int(3),
 email varchar(50) unique,
-phoneNumber int(10),
+phone_number long,
 password varchar(50),
 gender enum("Male","Female","Other"),
 role varchar(20),
 constraint employee_pk primary key(id)
 );
 
-insert into employee(id,name,age,email,phoneNumber,password,gender,role) values (1,'anjali','24','anjalidandriyal23@gmail.com','7535929032','anjali@123','F','admin');
+insert into employee(id,name,age,email,phone_number,password,gender,role) values (1,'abhishek','24','abhi.bhatt1857@gmail.com',7466992636,'Abhi@123','Male','admin');
+select * from employee;
